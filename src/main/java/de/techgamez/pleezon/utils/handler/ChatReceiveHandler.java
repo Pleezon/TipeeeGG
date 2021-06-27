51e7195cd6f2556ed5646ff5a875f98fb991a1c0
tom <tomhaas0212@gmail.com>
2021-06-27 15:13:26 +0200

    fixed another money bug

--- src/main/java/de/techgamez/pleezon/utils/handler/ChatReceiveHandler.java
+++ src/main/java/de/techgamez/pleezon/utils/handler/ChatReceiveHandler.java
@@ -6,11 +6,11 @@ import net.labymod.api.events.MessageReceiveEvent;
 import java.util.regex.Pattern;
 
 public class ChatReceiveHandler implements MessageReceiveEvent {
-    public final Pattern paymentValid = Pattern.compile(".+┃ §r[§0-9a-zA-Z!~_]+§r §r§ahat dir $[0-9.,]+ gegeben.§r");
+    public final Pattern paymentValid = Pattern.compile(".+┃ §r[§0-9a-zA-Z!~]+§r §r§ahat dir \\$[0-9.,]+ gegeben.§r");
     @Override
     public boolean onReceive(String raw, String text) {
         if(!TipeeeGG.isEnabled)return false;
-        if (raw.contains("§r §r§ahat dir ") && raw.endsWith("gegeben.§r") && !raw.contains("»") && paymentValid.matcher(raw).find() && !text.contains("mir")) {
+        if (paymentValid.matcher(raw).find() && !text.contains("mir")) {
             String name = text.split(" ")[2];
             double amount = Double.parseDouble(text.split(" ")[5].replace("$", "").replaceAll(",", ""));
             MoneyReceivedHandler.onMoneyReceived(amount,name);
